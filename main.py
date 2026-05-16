894d5498a572d1b2f41d859aa4bd1a875eac76a9
anibalsande <anibal.sande@udc.es>
2026-05-16 21:23:29 +0200

    Ajuste de prueba

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from plots import (plot_single_loss, plot_single_accuracy,
                    plot_comparison_table, show_qualitative_all)
 
 SEED = 42
-NUM_EPOCHS = 150
+NUM_EPOCHS = 10
 BATCH_SIZE = 4
 LR = 1e-4
 POS_WEIGHT = 8.0
